e4c5ca481c291d85c6016c5a7ff8fc22c9336ba8
Martin Zittel <martin.zittel@gmail.com>
2013-07-17 12:34:37 +0200

    foo

--- plastic-sense.c
+++ plastic-sense.c
@@ -43,9 +43,6 @@
 #include "contiki-net.h"
 #include "ringbuf.h"
 
-#include "lib/list.h"
-#include "lib/memb.h"
-
 #include <string.h>
 
 #include <stdio.h>
@@ -70,13 +67,13 @@ static struct psock ps;
 static char psock_RxBuffer[8];
 
 void callback(uint8_t d);
+
+// Parser
 void addConnection(uint16_t);
 void removeConnection(void *n);
 
 
-struct connection_list_struct {
-	struct connection_list_struct *next;
-  
+struct connection_struct {
 	uint8_t tcpRx_Buffer[128];
 	uint16_t port;
 	uint8_t isServer;
@@ -85,9 +82,8 @@ struct connection_list_struct {
 
 #define PACKET_TIMEOUT 60 * CLOCK_SECOND
 #define MAX_CONNECTIONS 16
-LIST(connection_list);
-MEMB(connection_mem, struct connection_list_struct, MAX_CONNECTIONS);
-                                            
+
+struct connection_struct connections[MAX_CONNECTIONS];                                     
 
 /*---------------------------------------------------------------------------*/
 static 
@@ -115,50 +111,50 @@ PROCESS_THREAD(plastic_sense_process, ev, data)
   	ringbuf_init(&serialRx_Buffer, serialRx, sizeof(serialRx));
 	
 	
-	list_init(connection_list);
-	
 	tcp_listen(UIP_HTONS(1010));
 
   	leds_on(LEDS_ALL);
 
 	while(1) {
 	
+		PROCESS_WAIT_EVENT();
 		
 		int i = 0;
-		struct connection_list_struct *n;
-		for(n = list_head(connection_list); n != NULL && i != list_length(connection_list); n = n->next) {
+		for(i = 0; i < MAX_CONNECTIONS; i++) {
+		
 			if(n->port == uip_conn){
 			
 				// TODO Handle Connection
 			
+			
+			if(ev == tcpip_event && !connected){
+				if(uip_connected()) {
+					PSOCK_INIT(&ps, psock_RxBuffer, sizeof(psock_RxBuffer));
+				}
+				connected = true;
 			}
+			else if(ev == tcpip_event && connected){
+				if(!(uip_aborted() || uip_closed() || uip_timedout())){
+					handle_connection(&ps);
+				}
+				else{
+					connected = false;
+				}
+			} else{
+				while(ringbuf_elements(&serialRx_Buffer) && transmissionPointer < sizeof(transmissionBuffer)){
+							transmissionBuffer[transmissionPointer]  = ringbuf_get(&serialRx_Buffer);
+							transmissionPointer++;
+				}
+			}
+			}
+				
 		}
 	
 	
 	
 	
 		
-		PROCESS_WAIT_EVENT();
 		
-		if(ev == tcpip_event && !connected){
-			if(uip_connected()) {
-				PSOCK_INIT(&ps, psock_RxBuffer, sizeof(psock_RxBuffer));
-			}
-			connected = true;
-		}
-		else if(ev == tcpip_event && connected){
-			if(!(uip_aborted() || uip_closed() || uip_timedout())){
-				handle_connection(&ps);
-			}
-			else{
-				connected = false;
-			}
-		} else{
-			while(ringbuf_elements(&serialRx_Buffer) && transmissionPointer < sizeof(transmissionBuffer)){
-        	        	transmissionBuffer[transmissionPointer]  = ringbuf_get(&serialRx_Buffer);
-        	        	transmissionPointer++;
-        		}
-		}
   	}	
 
 	PROCESS_END();
