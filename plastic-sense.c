daef3cddf9b4158e03757a684a987e99b3ab9523
Martin Zittel <martin.zittel@gmail.com>
2013-08-12 15:46:01 +0200

    further handle_connection implementation

--- plastic-sense.c
+++ plastic-sense.c
@@ -54,6 +54,19 @@
 #define CLIENT_NOT_CONNECTED 2
 #define CLIENT_CONNECTED 3
 
+#define STATE_FREE 0
+#define STATE_IDLE 1
+#define STATE_RECEIVING_TCP_DATA 2
+#define STATE_ARDUINO_CALLBACK_TIMED_OUT 3
+
+
+#define TCP_RX_BUFFER_SIZE 128
+#define TCP_TX_BUFFER_SIZE 128
+
+
+#define PACKET_TIMEOUT 60 * CLOCK_SECOND
+#define MAX_CONNECTIONS 16
+#define MAX_SERVER 4
 /*---------------------------------------------------------------------------*/
 uint8_t serialRx[128]; // need size of power two!
 struct ringbuf serialRx_Buffer;
@@ -66,6 +79,7 @@ void callback(uint8_t d);
 void clearConnection(struct connection *c);
 void tcp_appcall(void *state);
 void serial_appcall(void *state);
+void callArduino(struct connection *c);
 
 // Parser
 void addConnection(uint16_t);
@@ -76,21 +90,20 @@ struct arduinoServer {
 };
 
 struct connection {
-	bool free = true;
+	uint8_t state = STATE_FREE;
 	unsigned char timer;
 	// Psock Read Buffer
-	uint8_t tcpRx_Buffer[128];
+	uint8_t tcpRx_Buffer[TCP_RX_BUFFER_SIZE];
 	uint8_t tcpRx_Pointer = 0;
 	
-	uint8_t tcpTx_Buffer[128];
+	uint8_t tcpTx_Buffer[TCP_TX_BUFFER_SIZE];
 	uint8_t tcpTx_Pointer = 0;
 	
 	struct psock ps_in, ps_out;
 };
 
-#define PACKET_TIMEOUT 60 * CLOCK_SECOND
-#define MAX_CONNECTIONS 16
-#define MAX_SERVER 4
+
+time_t tcpReceiveStartTime = 0;
 
 struct connection connections[MAX_CONNECTIONS];                                     
 struct arduinoServer arduinoServers[MAX_SERVER];    
@@ -104,9 +117,34 @@ PT_THREAD(handle_input(struct connection *c))
 	
 	while(1){
 		PSOCK_WAIT_UNTIL(p, PSOCK_NEWDATA(s));
-   
-		PSOCK_READBUF_LEN(p, 1);
-		c->tcpRx_Pointer++;
+		
+		// if something arrived notify the Arduino
+		if(uip_len > 0){
+			callArduino(c);
+		
+		
+			if(uip_len > (TCP_RX_BUFFER_SIZE - c->tcpRx_Pointer) && ! c->state == STATE_ARDUINO_CALLBACK_TIMED_OUT)
+			{
+				c->state = STATE_RECEIVING_TCP_DATA;
+			}
+			else
+			{
+				// memcpy( &dst[dstIdx], &src[srcIdx], numElementsToCopy * sizeof( Element ) );
+				memcpy(c->tcpRx_Buffer[c->tcpRx_Pointer], uip_buf, uip_len);
+				c->tcpRx_Pointer = c->tcpRX_Pointer + uip_len;
+
+				c->state = STATE_IDLE;
+			}
+	
+		}
+
+		// uip_len > rxpuffer
+		// dann über die serielle schnittstele Arduino Callback versenden.
+		
+		// Wenn der Arduino nach den Daten der Verbindung gerfragt hat direkt seriell raussenden.
+
+		
+
 			
 	}
 	
@@ -133,10 +171,26 @@ PT_THREAD(handle_output(struct connection *c))
 }
 /*---------------------------------------------------------------------------*/
 static void
-handle_connection(struct httpd_state *s)
+handle_connection(struct connection *c)
 {
-	handle_input(s);
-	handle_output(s);
+	// TODO while State == warte noch auf Daten auf einer TCP Verbindung
+	// time_t myTime = clock_time(); 
+	// watchdog_periodic() Wenn der Timeout über 1 Sekunde dauert (1000* Clock_Seconds).
+	// Wenn Timeout dann aus der handle_connection rausspringen
+	
+	if(tcpReceiveStartTime == 0) tcpReceiveStartTime = clock_time();
+
+
+	do{
+		if(clock_time() - tcpReceiveStartTime > 500) c->state = STATE_ARDUINO_CALLBACK_TIMED_OUT;
+
+		handle_input(c);
+		handle_output(c);
+
+		watchdog_periodic();
+	} while (c->state == STATE_RECEIVING_TCP_DATA);
+
+	tcpReceiveStartTime = 0;
 }
 /*---------------------------------------------------------------------------*/
 PROCESS(plastic_sense_process, "plastic sense process");
@@ -156,15 +210,17 @@ PROCESS_THREAD(plastic_sense_process, ev, data)
 		if(ev == tcpip_event){
 			tcp_appcall(data);
 		}
-		else{
+		else if (ev == PROCESS_EVENT_POLL){
 			//handle Serial callback
 			serial_appcall(data);
+			// handle_output() mit der richtigen Verbindung aufrufen -> nicht notwendig da serial_appcall die Daten aus uip_buf versendet wenn die bereffende Verbindung noch in STATE_RECEIVING_TCP_DATA ist. 
 		}
 	}
 
 	PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
+// Handle input on the serial line.
 void callback(uint8_t d){
          ringbuf_put(&serialRx_Buffer, d);
          process_poll(&plastic_sense_process);
@@ -183,8 +239,8 @@ void tcp_appcall(void *state){
 		int i = 0;
 		// Add new Connection (= mark one free Connection as non-free)
 		for(i = 0; i < MAX_CONNECTIONS; i++) {
-			if(connections[i]->free == true){
-				connections[i]->free = false;
+			if(connections[i]->state == STATE_FREE){
+				connections[i]->state = STATE_IDLE;
 				c = &connections[i];
 			}
 		}
@@ -197,19 +253,14 @@ void tcp_appcall(void *state){
 		PSOCK_INIT(&c->ps_in, (uint8_t *)c->tcpRx_Buffer, sizeof(c->tcpRx_Buffer) - 1);
 		PSOCK_INIT(&c->ps_out, (uint8_t *)c->tcpRx_Buffer, sizeof(c->tcpRx_Buffer) - 1);
 		
-		c->timer = 0;
 		handle_connection(c);
 	} else if(c != NULL) {
-		// check if call came from uip-poll
-		if(uip_poll()) {
-		  ++c->timer;
-		  if(c->timer >= 20) {
+		
+		if(c->state == STATE_FREE){
 			uip_abort();
-			clearConnection(c);
-		  }
-		} else {
-		  c->timer = 0;
+			return;
 		}
+		
 		handle_connection(c);
 	} else {
 		uip_abort();
@@ -217,11 +268,11 @@ void tcp_appcall(void *state){
 }
 /*---------------------------------------------------------------------------*/
 void clearConnection(struct connection *c){
-	c->free = true;
-	c->timer = 0;
-	// TODO
-	c->tcpRx_Buffer = NULL;
-	c->ps = NULL;
+	c->state = STATE_FREE;
+	// TODO check
+	c->tcpRx_Pointer = 0;
+	c->tcpTx_Pointer = 0;
+
 }
 /*---------------------------------------------------------------------------*/
 void serial_appcall(data){
@@ -232,24 +283,28 @@ void serial_appcall(data){
 	decode serial line commands
 	 |-> add/remove connections
 	 |-> Add something to the tcp_Tx Buffers
-	 |-> Read sometrhin from the tcp_Rx Buffers
+	 |-> Read something from the tcp_Rx Buffers
+		-> check if any connection State is STATE_RECEIVING_TCP_DATA if so send Arduino the connection->tcpRx_Buffer PLUS the uip_buf (set the connection state to idle and the pointers to 0)
 	 
 	 */
 
 }
 
+/*---------------------------------------------------------------------------*/
 
 void addConnection(uint8_t isServer, uint16_t port, uip_ipaddr_t addr){
-	
-	
-	
-	if(isServer == 1) tcp_listen(UIP_HTONS(port));
-	else tcp_connect(addr, UIP_HTONS(port), NULL);
-	
 
+	if(isServer == 1){
+		tcp_listen(UIP_HTONS(port));
+	}
+	else{
+		tcp_connect(addr, UIP_HTONS(port), NULL); // in Fehlerfall gibt tcp_connect 0 zurück
+	}
+	
 }
-void removeConnection(void *n)
-{
-  
+/*---------------------------------------------------------------------------*/
+void callArduino(struct connection *c){
+	// Send Message to Arduino that some Data arrived via TCP
 }
+/*---------------------------------------------------------------------------*/
 
